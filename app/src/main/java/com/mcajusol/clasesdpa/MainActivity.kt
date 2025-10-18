9bed8edd3534a183bbd099c547ec4a77c1ebafc2
Marco <mcajusol@gmail.com>
2025-10-18 16:45:48 -0500

    Initial commit

---
+++ app/src/main/java/com/mcajusol/clasesdpa/MainActivity.kt
@@ -0,0 +1,31 @@
+package com.mcajusol.clasesdpa
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import com.mcajusol.clasesdpa.presentation.auth.LoginScreen
+import com.mcajusol.clasesdpa.presentation.auth.RegisterScreen
+import com.mcajusol.clasesdpa.ui.theme.ClasesDPATheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+            ClasesDPATheme {
+                // A surface container using the 'background' color from the theme
+                //LoginScreen()
+                RegisterScreen()
+
+            }
+        }
+    }
+}
---
+++ app/src/main/java/com/mcajusol/clasesdpa/presentation/auth/LoginScreen.kt
@@ -0,0 +1,72 @@
+package com.mcajusol.clasesdpa.presentation.auth
+
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Button
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.OutlinedTextField
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+
+
+@Composable
+@Preview(showBackground = true)
+fun LoginScreen(){
+
+    var email by remember { mutableStateOf("") }
+    var password by remember { mutableStateOf("") }
+
+
+    Column(
+
+        modifier = Modifier.padding(16.dp)
+
+    ){
+
+        Spacer(modifier = Modifier.height(16.dp))
+
+        Text(text = "Iniciar Sesión",style = MaterialTheme.typography.titleLarge)
+
+        OutlinedTextField(
+            value = email,
+            onValueChange = { email = it },
+            label = { Text("Correo electrónico") },
+            modifier = Modifier.padding(vertical = 8.dp).fillMaxWidth()
+
+        )
+         // OutlinedTextField -- para el passoword
+        OutlinedTextField(
+            value = password,
+            onValueChange = { password = it },
+            label = { Text("Contraseña") },
+            modifier = Modifier.padding(vertical = 8.dp).fillMaxWidth()
+
+        )
+
+        // Botón de inicio de sesión
+        Spacer(modifier = Modifier.height(16.dp))
+
+        Button(
+            onClick = { /* Acción al hacer clic en el botón */ },
+            modifier = Modifier.padding(vertical = 8.dp).fillMaxWidth()
+
+        ) {
+             Text("Iniciar Sesión")
+        }
+
+
+    }
+
+
+}
